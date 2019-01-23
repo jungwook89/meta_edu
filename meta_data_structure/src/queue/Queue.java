8c7811cbd60ef4346516068f4dd8bc400c2283bb
Your Name <bonazo@naver.com>
2019-01-23 14:55:57 +0900

    큐 추가

--- meta_data_structure/src/queue/Queue.java
+++ meta_data_structure/src/queue/Queue.java
@@ -1,24 +1,62 @@
 package queue;
 
+import linkedList.Node;
+
 public class Queue {
 		private int size;
+		private int curSize;
 		private Node front;
 		private Node rear;
+		
 	public Queue(int size){
 		this.size=size;
-		public void enQue(String data){
-			
+		this.front = null;
+		this.rear = null;
+		this.curSize=0;
+		
+	}
+	public boolean isEmpty(){
+		return (front==null)&&(rear==null);
+	}
+	public void enQue(String data){
+		if(isEmpty()){
+			rear= new Node(data);
+			front = rear;
+			curSize+=1;
+		}else{
+			if(size==curSize){
+				System.out.println("큐가 가득찼습니다.");
+			}else{
+				Node tpNode = new Node(data);
+				rear.setPostNode(tpNode);
+				rear = tpNode;
+				curSize+=1;
+			}
+		}
+	}
+	public void deQue(){
+		if(front==rear){
+			System.out.println("큐가 비었습니다");
+		}else{
+			System.out.println(front.getData());
+			front = front.getPostNode();
 		}
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Queue test1 = new Queue(4);
-		test1.enQue(1);
-		test1.enQue(2);
+		Queue test1 = new Queue(5);
+		test1.enQue("1");
+		test1.enQue("2");
 		test1.deQue();
-		test1.enQue(3);
-		test1.enQue(4);
+		test1.enQue("3");
+		test1.enQue("4");
+		test1.enQue("5");
+		test1.enQue("6");
 		test1.deQue();
 		test1.deQue();
-
+		test1.deQue();
+		test1.deQue();
+		test1.deQue();
+		test1.deQue();
+	}
 }
